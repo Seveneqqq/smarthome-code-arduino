9d88a6770e033b0d7e12b7679297b4c6b8bcb5f1
Paweł Boroń <pawcio2001.boron@interia.pl>
2024-12-31 11:59:28 +0100

    Dodanie obslugi FRONT_GATE

--- node_to_arduino/node_to_arduino.ino
+++ node_to_arduino/node_to_arduino.ino
@@ -1,5 +1,6 @@
 #include <ArduinoJson.h>
 #include "DHT.h"
+#include <Servo.h>
 
 #define SERIAL_TX_BUFFER_SIZE 256
 #define SERIAL_RX_BUFFER_SIZE 256
@@ -7,6 +8,12 @@
 #define trigPin 3
 #define DHT11_PIN 7
 
+#define SERVO_PIN 9
+#define SERVO_PIN2 10
+#define SERVO_PIN3 11
+Servo myservo;
+Servo myservo2;
+Servo myservo3;
 DHT dht;
 
 long duration;
@@ -14,10 +21,14 @@ int distance;
 int lightPin = 5;
 int lightPin2 = 4;
 
-
 unsigned long previousMillis = 0;
 const long interval = 1000;
 bool isStarted = false;
+
+// Bufor na dane JSON
+char jsonBuffer[512];
+int bufferIndex = 0;
+bool jsonComplete = false;
 
 void setup() {
     pinMode(trigPin, OUTPUT);
@@ -26,132 +37,247 @@ void setup() {
     pinMode(lightPin2, OUTPUT);
     dht.setup(DHT11_PIN);
     Serial.begin(9600);
+    myservo.attach(SERVO_PIN);
+    myservo2.attach(SERVO_PIN2);
+    myservo3.attach(SERVO_PIN3);
+    while (!Serial) continue;
 }
 
 void loop() {
- 
-    delay(1000); //Odczekanie wymaganego czasu
 
-    if(isStarted == true){
-        delay(300);
-        startApp();
+    
+
+
+    if(isStarted) {
+        unsigned long currentMillis = millis();
+        if (currentMillis - previousMillis >= interval) {
+            previousMillis = currentMillis;
+            sendSensorData();
+        }
     }
 
-    if (Serial.available() > 0) {
-
-      delay(500); //bez tego delaya wariuje program 
-
-        String receivedString = "";
+    // Odczytaj dostępne dane
+    while (Serial.available() > 0) {
+        char inChar = (char)Serial.read();
         
-        while (Serial.available() > 0) {
-            receivedString += char(Serial.read());
-        }
-
-        StaticJsonDocument<800> doc;
-        DeserializationError error = deserializeJson(doc, receivedString);
-
-        if (error) {
-            Serial.println("JSON deserialization failed");
-            return;
-        }
-
-        String instruction = doc["instruction"].as<String>();
-        
-        if (instruction == "check-connection") {
-            digitalWrite(lightPin, HIGH);
-            digitalWrite(lightPin2, HIGH);
-            delay(1000);
-            digitalWrite(lightPin, LOW);
-            digitalWrite(lightPin2, LOW);
-            returnIsConnected();
-        }
-        if (instruction == "scenario") {
-          Serial.println("Wywolywanie");
-          handleScenario(doc);
-        }
-        if(instruction == "send-devices-list"){
-            sendDeviceList();
-        }
-        
-        if(instruction == "start-app"){
-            startApp();
-            isStarted = true;
-        }
-        
-        if(instruction == "device-control"){
-            String deviceName = doc["device"].as<String>();
-            int state = doc["actions"]["state"].as<int>();
-            int brightness = doc["actions"]["brightness"].as<int>();
-
-            if(deviceName == "LED1") {
-                if(state == 1){
-                    digitalWrite(lightPin, HIGH);
-                } else {
-                    digitalWrite(lightPin, LOW);
+        // Dodaj znak do bufora
+        if (bufferIndex < sizeof(jsonBuffer) - 1) {
+            jsonBuffer[bufferIndex] = inChar;
+            bufferIndex++;
+            
+            // Sprawdź, czy to koniec JSONa
+            if (inChar == '\n' || inChar == '\r') {
+                // Zakończ string
+                jsonBuffer[bufferIndex - 1] = '\0';
+                
+                // Znajdź początek i koniec JSON
+                char* start = strchr(jsonBuffer, '{');
+                char* end = strrchr(jsonBuffer, '}');
+                
+                if (start != NULL && end != NULL && end > start) {
+                    // Mamy kompletny JSON
+                    processJsonCommand(start);
                 }
+                
+                // Resetuj bufor
+                bufferIndex = 0;
+                memset(jsonBuffer, 0, sizeof(jsonBuffer));
             }
-            else if(deviceName == "LED2") {
-                if(state == 1){
-                    digitalWrite(lightPin2, HIGH);
-                } else {
-                    digitalWrite(lightPin2, LOW);
-                }
-            }
+        } else {
+            // Przepełnienie bufora - resetuj
+            bufferIndex = 0;
+            memset(jsonBuffer, 0, sizeof(jsonBuffer));
+            sendErrorResponse("Buffer overflow", "");
         }
     }
 }
 
-void handleScenario(StaticJsonDocument<800>& doc) {
+void processJsonCommand(const char* jsonString) {
+    // Debug - pokaż otrzymany JSON
+    StaticJsonDocument<200> debugDoc;
+    debugDoc["type"] = "debug";
+    debugDoc["received"] = jsonString;
+    String debugOutput;
+    serializeJson(debugDoc, debugOutput);
+    Serial.println(debugOutput);
 
-  Serial.println("Wywolywanie 2");
-    bool scenarioState = doc["state"];
-    JsonArray devices = doc["devices"];
+    // Parsuj JSON
+    StaticJsonDocument<800> doc;
+    DeserializationError error = deserializeJson(doc, jsonString);
 
-    for (JsonObject device : devices) {
-        String deviceName = device["name"].as<String>();
-        JsonObject actions = device["actions"];
-        int state = actions["state"];
-        int brightness = actions["brightness"];
+    if (error) {
+        char errorMsg[100];
+        snprintf(errorMsg, sizeof(errorMsg), "Deserializacja JSON nie powiodła się: %s", error.c_str());
+        sendErrorResponse(errorMsg, "");
+        return;
+    }
 
-        if (deviceName == "LED1") {
-            if (state == 1) {
-                digitalWrite(lightPin, HIGH);
-            } else {
-                digitalWrite(lightPin, LOW);
-            }
-        }
-        else if (deviceName == "LED2") {
-            if (state == 1) {
-                digitalWrite(lightPin2, HIGH);
-            } else {
-                digitalWrite(lightPin2, LOW);
-            }
-        }
+    // Sprawdź czy mamy wymagane pola
+    if (!doc.containsKey("instruction")) {
+        sendErrorResponse("Brak pola 'instruction'", "");
+        return;
+    }
+
+    const char* instruction = doc["instruction"] | "";
+    const char* commandId = doc["id"] | "";
+    
+    if (strcmp(instruction, "check-connection") == 0) {
+        digitalWrite(lightPin, HIGH);
+        digitalWrite(lightPin2, HIGH);
+        delay(1000);
+        digitalWrite(lightPin, LOW);
+        digitalWrite(lightPin2, LOW);
+        sendSuccessResponse(commandId, "connection_checked");
+    }
+    else if (strcmp(instruction, "scenario") == 0) {
+        handleScenario(doc);
+        sendSuccessResponse(commandId, "scenario_executed");
+    }
+    else if(strcmp(instruction, "send-devices-list") == 0) {
+        sendDeviceList();
+        sendSuccessResponse(commandId, "devices_list_sent");
+    }
+    else if(strcmp(instruction, "start-app") == 0) {
+        isStarted = true;
+        sendSuccessResponse(commandId, "app_started");
+    }
+    else if(strcmp(instruction, "device-control") == 0) {
+        handleDeviceControl(doc);
+        sendSuccessResponse(commandId, "device_controlled");
+    }
+    else {
+        char errorMsg[100];
+        snprintf(errorMsg, sizeof(errorMsg), "Nieznana instrukcja: %s", instruction);
+        sendErrorResponse(errorMsg, commandId);
     }
 }
 
-void startApp(){
+void handleDeviceControl(const JsonDocument& doc) {
+    const char* deviceName = doc["device"] | "";
+    int state = doc["actions"]["state"] | 0;
+    int brightness = doc["actions"]["brightness"] | 100;
 
+    if(strcmp(deviceName, "LED1") == 0) {
+        int pwmValue = map(brightness, 0, 100, 0, 255);
+        if(state == 1) {
+            analogWrite(lightPin, pwmValue);
+        } else {
+            analogWrite(lightPin, 0);
+        }
+    }
+    else if(strcmp(deviceName, "LED2") == 0) {
+        digitalWrite(lightPin2, state == 1 ? HIGH : LOW);
+    }
+    else if(strcmp(deviceName, "FRONT_GATE") == 0) {
+        if(state == 0){
+          myservo.write(162);
+          myservo2.write(43);
+          delay(500);
+        }
+        else{
+        myservo.write(62);
+        myservo2.write(140);
+        delay(500);
+        }
+    }
+    else if(strcmp(deviceName, "GARAGE_GATE") == 0) {
+        if(state == 0){
+          myservo3.write(10);
+          delay(500);
+        }
+        else{
+        myservo3.write(160);
+        delay(500);
+        }
+    }
+
+}
+
+void handleScenario(const JsonDocument& doc) {
+    bool scenarioState = doc["state"] | false;
+    JsonArrayConst devices = doc["devices"].as<JsonArrayConst>();
+
+    for (JsonVariantConst device : devices) {
+        const char* deviceName = device["name"] | "";
+        int state = device["actions"]["state"] | 0;
+        int brightness = device["actions"]["brightness"] | 100;
+
+        if(strcmp(deviceName, "LED1") == 0) {
+            int pwmValue = map(brightness, 0, 100, 0, 255);
+            if(state == 1) {
+                analogWrite(lightPin, pwmValue);
+            } else {
+                analogWrite(lightPin, 0);
+            }
+        }
+        else if(strcmp(deviceName, "LED2") == 0) {
+            digitalWrite(lightPin2, state == 1 ? HIGH : LOW);
+        }
+        else if(strcmp(deviceName, "FRONT_GATE") == 0) {
+          if(state == 0){
+          myservo.write(162);
+          myservo2.write(43);
+          delay(500);
+          }
+          else{
+          myservo.write(62);
+          myservo2.write(140);
+          delay(500);
+          }
+        }
+        else if(strcmp(deviceName, "GARAGE_GATE") == 0) {
+        if(state == 0){
+          myservo3.write(10);
+          delay(500);
+        }
+        else{
+        myservo3.write(160);
+        delay(500);
+        }
+    }
+    }
+}
+
+void sendSensorData() {
+    StaticJsonDocument<200> doc;
+    
     digitalWrite(trigPin, LOW);
-    delayMicroseconds(300);
-    digitalWrite(trigPin,HIGH);
-    delayMicroseconds(300);
-    digitalWrite(trigPin,LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
 
     duration = pulseIn(echoPin, HIGH);
     distance = duration * 0.0344 / 2;
 
-    int humidity = dht.getHumidity();
-    int temperature = dht.getTemperature();
+    doc["type"] = "sensor_data";
+    doc["distance"] = distance;
+    doc["humidity"] = dht.getHumidity();
+    doc["temperature"] = dht.getTemperature();
 
-    Serial.println("{ \"distance\": " + String(distance) + 
-                  ", \"humidity\": " + String(humidity) + 
-                  ", \"temperature\": " + String(temperature) + " }");
+    String jsonOutput;
+    serializeJson(doc, jsonOutput);
+    Serial.println(jsonOutput);
 }
 
-void returnIsConnected(){
-    StaticJsonDocument<800> doc;
-    doc["connected"] = true;
+void sendSuccessResponse(const char* commandId, const char* details) {
+    StaticJsonDocument<200> doc;
+    doc["type"] = "command_response";
+    doc["id"] = commandId;
+    doc["status"] = "success";
+    doc["details"] = details;
+    
+    String jsonOutput;
+    serializeJson(doc, jsonOutput);
+    Serial.println(jsonOutput);
+}
+
+void sendErrorResponse(const char* error, const char* commandId) {
+    StaticJsonDocument<200> doc;
+    doc["type"] = "command_response";
+    doc["id"] = commandId;
+    doc["status"] = "error";
+    doc["error"] = error;
     
     String jsonOutput;
     serializeJson(doc, jsonOutput);
@@ -159,8 +285,7 @@ void returnIsConnected(){
 }
 
 void sendDeviceList() {
-    StaticJsonDocument<800> doc; //rozmiar dokumentow, przy 200 wysyla 2 
-    
+    StaticJsonDocument<800> doc;
     JsonArray devices = doc.createNestedArray("devices");
 
     JsonObject device1 = devices.createNestedObject();
@@ -171,23 +296,19 @@ void sendDeviceList() {
     JsonObject device2 = devices.createNestedObject();
     device2["id"] = 2;
     device2["name"] = "LED2";
-    device2["status"] = "not-active";
-    
+    device2["status"] = "active";
+
     JsonObject device3 = devices.createNestedObject();
     device3["id"] = 3;
-    device3["name"] = "LED3";
+    device3["name"] = "FRONT_GATE";
     device3["status"] = "active";
 
     JsonObject device4 = devices.createNestedObject();
     device4["id"] = 4;
-    device4["name"] = "LED4";
-    device4["status"] = "not-active";
-   
-
+    device4["name"] = "GARAGE_GATE";
+    device4["status"] = "active";
+    
     String jsonOutput;
     serializeJson(doc, jsonOutput);
     Serial.println(jsonOutput);
 }
-
-
-
